e760a6d8e7e8350d8a1f3ceddb9c6e745fd40dc8
kontb2a22 <ahmedxd.2009@gmail.com>
2023-08-24 02:03:56 +0300

    add: new env varaible to test env variables

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
 import express from 'express';
+import dotenv from 'dotenv';
 const app = express();
+dotenv.config();
+
+const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-    res.send('Choo Choo! Welcome to your Express app 🚅');
+    res.send(`Choo Choo! Welcome to your Express app 🚅$on port ${port} AND ${process.env.MYAPI_KEY}`);
 })
 
 app.get("/json", (req, res) => {
     res.json({"Choo Choo": "Welcome to your Express app 🚅"});
 })
 
-const port = process.env.PORT || 3000;
+console.log(port);
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
